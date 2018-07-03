dacf18fb3c333b774accbbded8ca20acc85e7fd9
SDBagel <35588499+SDBagel@users.noreply.github.com>
2018-07-03 02:44:55 -0700

    Initial Changes
    Getting some base things set up like forms needed and names for them, etc.

--- Cynobroad/Client.Designer.cs
+++ Cynobroad/Client.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Cynobroad
 {
-    partial class Form1
+    partial class Client
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Client));
+            this.SuspendLayout();
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Client";
+            this.Text = "Cynobroad Client";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
