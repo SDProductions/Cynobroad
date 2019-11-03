0e316bfa3c9a19481fd729b6e06c2f5b65aa43cd
Kyle <sdbagel@gmail.com>
2019-11-03 15:56:31 -0800

    Scaffold custom Identity UI

---
+++ Continuum/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Continuum.Areas.Identity.Data;
+using Continuum.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Continuum.Areas.Identity.IdentityHostingStartup))]
+namespace Continuum.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
