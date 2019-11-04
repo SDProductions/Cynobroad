625c2024b8760c3a8635eba51fdd758932986f99
Kyle <sdbagel@gmail.com>
2019-11-03 20:33:46 -0800

    Login/registration with username
    Use username instead of email to login

--- Continuum/Startup.cs
+++ Continuum/Startup.cs
@@ -35,6 +35,17 @@ namespace Continuum
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 8;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
