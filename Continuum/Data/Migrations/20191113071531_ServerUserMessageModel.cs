d53b29773443b1d0b8abe4effd60fb7e9c52975e
Kyle <35588499+SDBagel@users.noreply.github.com>
2019-11-12 23:15:58 -0800

    Server-Users Model

---
+++ Continuum/Data/Migrations/20191113071531_ServerUserMessageModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Continuum.Data.Migrations
+{
+    public partial class ServerUserMessageModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Server",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OwnerID = table.Column<string>(nullable: true),
+                    ContinuumUserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Server", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Server_AspNetUsers_ContinuumUserId",
+                        column: x => x.ContinuumUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Message",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OwnerID = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Content = table.Column<string>(nullable: true),
+                    ServerId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Message", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Message_Server_ServerId",
+                        column: x => x.ServerId,
+                        principalTable: "Server",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OwnerID = table.Column<string>(nullable: true),
+                    Nickname = table.Column<string>(nullable: true),
+                    ServerId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_User_Server_ServerId",
+                        column: x => x.ServerId,
+                        principalTable: "Server",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Message_ServerId",
+                table: "Message",
+                column: "ServerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Server_ContinuumUserId",
+                table: "Server",
+                column: "ContinuumUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_ServerId",
+                table: "User",
+                column: "ServerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Message");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "Server");
+        }
+    }
+}
--- Continuum/Models/User.cs
+++ Continuum/Models/User.cs
@@ -1,17 +1,15 @@
-﻿using Continuum.Areas.Identity.Data;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Continuum.Models
 {
-    public class Server
+    public class User
     {
         public int Id { get; set; }
 
         public string OwnerID { get; set; }
-        public List<User> Users { get; set; }
-        public List<Message> Messages { get; set; }
+        public string Nickname { get; set; }
     }
 }
